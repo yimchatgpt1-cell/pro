fbfb67ef3597a6ab64282202e5db8357b2a5e862
yimchatgpt1-cell <yimchatgpt1@gmail.com>
2026-03-18 11:23:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,10 @@ def analyze_food():
         # 1. เรียก Classification (หาชื่อเมนู)
         class_result = CLIENT.infer(temp_path, model_id="menu-a4fzp/3")
         eng_menu = class_result['predictions'][0]['class'] if class_result.get('predictions') else "ไม่มีเมนูอาหารนี้ในระบบ"
+        if top_prediction['confidence'] > 0.5:
+            menu_name = top_prediction['class']
+        else:
+            menu_name = "ไม่มีข้อมลูในระบบ"
 
         # 2. เรียก Object Detection (หาวัตถุดิบ)
         detect_result = CLIENT.infer(temp_path, model_id="ingredient_detection-vouls/35")
